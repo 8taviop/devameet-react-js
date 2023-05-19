f80ea0ae153340b89e01029cf5688f858f900b58
Hotávio Pacheco <8taviop@gmail.com>
2023-05-19 18:31:01 -0300

    adição das reuniões

---
+++ src/views/MeetAdd.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../components/general/Footer";
+import { Header } from "../components/general/Header";
+import { MeetAdd } from "../components/meet/MeedAdd";
+
+export const MeetAddView = () => {
+    return(
+        <>
+            <Header />
+            <MeetAdd />
+            <Footer />
+        </>
+    );
+}
