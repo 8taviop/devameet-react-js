0ce6aeee9e4fd264fddd3443790824ef9db0df66
Hotávio Pacheco <8taviop@gmail.com>
2023-05-19 14:46:17 -0300

    exclusão de reunião

--- src/services/MeetServices.ts
+++ src/services/MeetServices.ts
@@ -2,9 +2,13 @@ import { HttpApiServices } from "./HttpApiService";
 
 export class MeetServices extends HttpApiServices {
     
-    baseUrl = 'meet';
+    baseUrl = '/meet';
 
     async getMeets(){
         return await this.get(this.baseUrl);
     }
+
+    async deleteMeet(id: string){
+        return await this.delete(this.baseUrl+'/'+id);
+    } 
 }
